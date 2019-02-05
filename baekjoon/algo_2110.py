c61cb8d5b8fcd5131f3e54f8ee0e8660a8474925
Park Hyun Seo <hyeonseo9669@hanmail.net>
2019-02-05 08:59:48 +0000

    feat: Add BOJ 2110 problem

--- baekjoon/algo_2110.py
+++ baekjoon/algo_2110.py
@@ -1,15 +1,33 @@
-import bisect
+N, M = map(int, input().split())
 
-N, M = map(int, raw_input().split())
 houses = []
-
-for i in range(M):
-    houses.append(input())
+for i in range(N):
+    houses.append(int(input()))
 
 houses.sort()
 
-#def solve(fm, to, count):
-#    i = bisec.bisec(houses, (houses[fm] + houses[to]) / 2)
+ans = 0
+
+left = 1
+right = houses[-1] - houses[0]
+
+while left <= right:
+    mid = (left + right) >> 1
     
-#solove(0, N-1)
+    #print(left, mid, right)
     
+    before = houses[0]
+    count = 1
+    
+    for i in range(1, N):
+        if houses[i] >= before + mid:
+            count += 1
+            before = houses[i]
+    
+    if count < M: # 차이가 너무 크면
+        right = mid - 1
+    else:
+        ans = mid
+        left = mid + 1
+
+print(ans)
